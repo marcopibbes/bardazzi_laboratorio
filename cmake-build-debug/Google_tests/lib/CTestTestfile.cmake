f15bbe608ddffa4b9f7c31394bcdb1eabc0652ad
marcopibbes <marco.bardazzi1@stud.unifi.it>
2022-05-04 17:54:09 +0200

    finished unit testing

---
+++ cmake-build-debug/Google_tests/lib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mpibbes/Scrivania/C++/bardazzi_lab/Google_tests/lib
+# Build directory: /home/mpibbes/Scrivania/C++/bardazzi_lab/cmake-build-debug/Google_tests/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ cmake-build-debug/Google_tests/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mpibbes/Scrivania/C++/bardazzi_lab/Google_tests/lib/googletest
+# Build directory: /home/mpibbes/Scrivania/C++/bardazzi_lab/cmake-build-debug/Google_tests/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
